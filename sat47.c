618372db9b7198e4905d9843869e9f3417a3b666
sathishmech <30343187+sathishmech@users.noreply.github.com>
2017-08-22 16:19:02 +0530

    Create sat47.c

---
+++ sat47.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the value");
+scanf("%d",&a);
+if(a>0)
+{
+printf("THE VALUE IS POSITIVE");
+}
+else if(a<0)
+{
+printf("THE VALUE IS NEGATIVE");
+}
+else
+{
+printf("THE VALUE IS ZERO");
+}
+}
